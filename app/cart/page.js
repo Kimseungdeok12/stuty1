18b75f24e28ded237941ecc6b6f901461a925fc1
KimSeungDeok <sdkksd6331@gmail.com>
2024-07-09 09:52:33 +0900

    m7d9next1

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,20 +1,43 @@
+import { redirect } from "next/dist/server/api-utils/index.js"
+
+let 장바구니 = ['Tomatoes', 'paste']
+
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'paste']
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem/>
-      <CartItem/>
-      <CartItem/>
+      <CartItem 작명 = {장바구니[0]}/>
+      <CartItem 작명 = {장바구니[1]}/>
+      <Banner content = '롯데카드'/>
+      <Banner content = '현대카드'/>
+      <Button colors = 'blue'/>
     </div>
   )
 } 
 
-function CartItem() {
+function CartItem(props) {
   return (
     <div className="cart-item">
-        <p>상품명</p>
+        <p>{props.작명}</p>
         <p>$40</p>
         <p>1개</p>
       </div>
   )
-})
+}
+
+function Banner(props) {
+  return (
+    <div>
+      {props.content} 결제 행사중
+    </div>
+  )
+}
+
+function Button(props) {
+  return (
+    <div>
+      <button style={{background: props.colors}}>버튼임</button>
+    </div>
+  )
+}
